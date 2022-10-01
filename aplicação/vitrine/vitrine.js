21d0b68ce650017f7d225a5696ae61dbd33e8e4b
Nicholas Zortea Graczik <78584142+NicholasZortea@users.noreply.github.com>
2022-09-30 22:03:54 -0300

    Mudança nmr celular

--- aplicação/vitrine/vitrine.js
+++ aplicação/vitrine/vitrine.js
@@ -8,6 +8,6 @@ function enviarOrcamento() {
     const orcamento = document.querySelector("#textaraMensagem").value;
 
     const mensagem = orcamento.replace(/(\r\n|\n|\r)/gm, "%0A%0D");
-
-    window.open('https://wa.me/5554992226444?text=' + 'Meu nome e: ' + nome + '. Gostaria de orcar os seguintes itens: %0A%0D' + mensagem);
+    /*substituir o numero do link abaixo "000000000" para um numero de celular que existe*/
+    window.open('https://wa.me/5554000000000?text=' + 'Meu nome e: ' + nome + '. Gostaria de orcar os seguintes itens: %0A%0D' + mensagem);
 }
